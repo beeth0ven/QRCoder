be7ffeb8bbcd8f475b59fdbdea78c876ebc66722
beeth0ven <beeth0vendev@163.com>
2017-09-18 15:36:59 +0800

    Add RxFeedback+ActionState

---
+++ Internal/RxFeedback+/ActionFeedback.swift
@@ -0,0 +1,90 @@
+//
+//  ActionFeedback.swift
+//  Internal
+//
+//  Created by luojie on 2017/9/18.
+//  Copyright © 2017年 LuoJie. All rights reserved.
+//
+
+import RxSwift
+import RxCocoa
+import RxFeedback
+
+public enum ActionState<Input, Output> {
+    case empty
+    case triggered(Input)
+    case success(Output)
+    case error(Swift.Error)
+    
+    public enum Event {
+        case trigger(Input)
+        case result(Result<Output>)
+    }
+    
+    public typealias State = ActionState<Input, Output>
+    public typealias Feedback = (ObservableSchedulerContext<State>) -> Observable<Event>
+
+    private static func reduce(_ state: State, event: Event) -> State {
+        switch event {
+        case .trigger(let input):
+            return .triggered(input)
+        case .result(.success(let output)):
+            return .success(output)
+        case .result(.failure(let error)):
+            return .error(error)
+        }
+    }
+    
+    public static func system(
+        inputs: Observable<Input>,
+        workFactory: @escaping (Input) -> Observable<Output>,
+        scheduler: ImmediateSchedulerType
+        ) -> Observable<State> {
+        
+        let trigger: Feedback = { state in
+            state.flatMapLatest { state -> Observable<Event> in
+                if state.executing {
+                    return .empty()
+                }
+                return inputs.map(Event.trigger)
+            }
+        }
+        
+        let reactInput: Feedback = react(query: { $0.input }, effects: { input in
+            workFactory(input)
+                .mapToResult()
+                .map(Event.result)
+        })
+        
+        return Observable.system(
+            initialState: State.empty,
+            reduce: State.reduce,
+            scheduler: scheduler,
+            scheduledFeedback:
+                trigger,
+                reactInput
+        )
+        
+    }
+}
+
+extension ActionState {
+    
+    public var executing: Bool {
+        switch self {
+        case .triggered:
+            return true
+        default:
+            return false
+        }
+    }
+    
+    public var input: Input? {
+        switch self {
+        case .triggered(let input):
+            return input
+        default:
+            return nil
+        }
+    }
+}
